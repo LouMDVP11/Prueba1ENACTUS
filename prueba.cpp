16d9bfcade1b2e3131bbd3511a0e5517e801a33d
LouMDVP11 <66640684+LouMDVP11@users.noreply.github.com>
2020-06-09 21:13:32 -0600

    Create prueba.cpp

---
+++ prueba.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    printf("WENAAASS\nEste es el mi primer proyecto en Visual Code");
+    int num1, num2;
+    printf("Ingresa un numero: ");scanf("%d", num1);
+    printf("Ingresa otro numero: ");scanf("%d", num2);
+    printf("\nLa suma de tus numeros es: %d\n\n", num1 + num2)
+    system("pause");
+}
